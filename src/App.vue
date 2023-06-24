f89c6b1b812315107199491de5c7d1ded6cbc010
Buttlu <jackan202@gmail.com>
2023-06-25 01:50:43 +0200

    Table working

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import json from './json/data.json' //ignore, it works
+export default {
+    data() {
+        return {
+            jsonData: json
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <table border="1">
+            <tr v-for="data in jsonData">
+                <td>ID: {{ data.stateid }}</td>
+                <td>Name: {{ data.name }}</td>
+                <td>Pop: {{ new Intl.NumberFormat('se-SE').format(+data.population) }}</td>
+                <td style="padding: .1em"><img :src="data.flag" /></td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+}
+
+td {
+    padding: .3em;
+}
+
+table {
+    border-collapse: collapse;
+    padding: .3em;
+    margin: 0 auto;
+}
+</style>
+<style>
+#app {
+    display: flex;
+    justify-content: center;
+}
+</style>
